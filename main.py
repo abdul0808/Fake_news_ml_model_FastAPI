2d63435fe921cbaac416106a27db33cf237df5ed
abdul0808 <77425399+abdul0808@users.noreply.github.com>
2023-12-10 22:41:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 12 11:36:48 2022
+# # -*- coding: utf-8 -*-
+# """
+# Created on Sun Jun 12 11:36:48 2022
 
-@author: abdul
-"""
+# @author: abdul
+# """
+
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from pydantic import BaseModel
+# import pickle
+# import json
+
+
+# app = FastAPI()
+
+# origins = ["*"]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# class model_input(BaseModel):
+    
+#     news_heading: str
+    
+
+# # loading the saved model
+# fakenews_model = pickle.load(open('FakeNews_model.sav','rb'))
+
+
+# @app.post('/FakeNews_prediction')
+# def diabetes_pred(input_parameters : model_input):
+    
+#     input_data = input_parameters.json()
+#     input_dictionary = json.loads(input_data)
+    
+#     news = input_dictionary['news_heading']
+    
+
+
+#     input_list = [news]
+    
+#     prediction = fakenews_model.predict([input_list])
+    
+#     if prediction[0] == 0:
+#         return 'The news is Real'
+    
+#     else:
+#         return 'The news is Fake'
+
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
 import json
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 app = FastAPI()
 
@@ -24,33 +76,42 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class model_input(BaseModel):
-    
+class ModelInput(BaseModel):
     news_heading: str
     
+# Load the saved model
+fakenews_model = pickle.load(open('FakeNews_model1.sav', 'rb'))
 
-# loading the saved model
-fakenews_model = pickle.load(open('FakeNews_model.sav','rb'))
+# Load the TF-IDF vectorizer
+vectorizer = pickle.load(open('TfidfVectorizer_model.sav', 'rb'))
 
+port_stem = PorterStemmer()
+
+def preprocess_text(content):
+    # Apply stemming and other preprocessing steps
+    stemmed_content = re.sub('[^a-zA-Z]', ' ', content)
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [port_stem.stem(word) for word in stemmed_content if not word in stopwords.words('english')]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
 
 @app.post('/FakeNews_prediction')
-def diabetes_pred(input_parameters : model_input):
-    
-    input_data = input_parameters.json()
-    input_dictionary = json.loads(input_data)
-    
-    news = input_dictionary['news_heading']
-    
+def fake_news_prediction(input_parameters: ModelInput):
+    # Preprocess the input text
+    news_heading = preprocess_text(input_parameters.news_heading)
 
+    # Vectorize the preprocessed text
+    input_vector = vectorizer.transform([news_heading])
 
-    input_list = [news]
-    
-    prediction = fakenews_model.predict([input_list])
-    
+    # Make predictions using the loaded model
+    prediction = fakenews_model.predict(input_vector)
+
+    # Return the prediction result
     if prediction[0] == 0:
-        return 'The news is Real'
-    
-    else:
         return 'The news is Fake'
+    else:
+        return 'The news is Real'
+
 
 
